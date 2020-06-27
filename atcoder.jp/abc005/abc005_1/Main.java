74688c4c04ea3979dc8af211c7801912fce1038b
nemurinda <nemurinda0212@gmail.com>
2020-06-27 13:37:12 +0900

    [AC] abc005 abc005_1

---
+++ atcoder.jp/abc005/abc005_1/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String[] str = in.readLine().split(" ");
+    int x = Integer.parseInt(str[0]);
+    int y = Integer.parseInt(str[1]);
+    System.out.println(y/x);
+  }
+}
