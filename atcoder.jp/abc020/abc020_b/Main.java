5354ba78f6116f54f1a95eea11f531136c993461
nemurinda <nemurinda0212@gmail.com>
2020-06-27 16:25:47 +0900

    [AC] abc020 abc020_b

---
+++ atcoder.jp/abc020/abc020_b/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String[] str = in.readLine().split(" ");
+    int ab = Integer.parseInt(str[0] + str[1]);
+    System.out.println(ab * 2);
+  }
+}
