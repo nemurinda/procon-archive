6b56cbbf6dddb39932cffb95749b9207626a12de
nemurinda <nemurinda0212@gmail.com>
2020-06-27 09:42:06 +0900

    [AC] abc023 abc023_a

---
+++ atcoder.jp/abc023/abc023_a/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    String[] str = in.readLine().split("");
+    System.out.println(Integer.parseInt(str[0]) + Integer.parseInt(str[1]));
+  }
+}
+    
