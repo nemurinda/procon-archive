445764b00d7bab2a2e934b647c10e17102df1de5
nemurinda <nemurinda0212@gmail.com>
2020-07-05 21:02:40 +0900

    [AC] abc173 abc173_a

---
+++ atcoder.jp/abc173/abc173_a/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(in.readLine());
+    System.out.println(n%1000==0?0:Math.abs(1000-n%1000));
+  }
+}
